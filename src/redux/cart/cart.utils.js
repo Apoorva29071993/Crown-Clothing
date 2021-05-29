91e52a2e2e8ee4dfa8e3552b76c92da9d949fc0a
Aishwarya-Jakati <jakati.aishwaryaa97@gmail.com>
2021-05-29 15:45:34 +0530

    created cart item component and connected our cart dropdown with cartitems reducer

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,8 +1,15 @@
 export const addItemToCart = (cartItems , cartItemToAdd) => {
+
+    // Check if the item is already present in the cart items 
+    // If the item is present in the cartItems array use map function to iterate all the items in the array
+    // So if the id matches return the object with that item by increase the quantity
+    // If the id doesnot match return the item as it is into the cartItems array
+    
+    // If the item is not present in the cartItems array skip if block , return array with old cart items and add the new item 
+    // with quantity as one
     const existingCartItem = cartItems.find(
         cartItem => cartItem.id  === cartItemToAdd.id
         );
-
      if (existingCartItem) {
          return cartItems.map(cartItem => 
             cartItem.id === cartItemToAdd.id
@@ -14,3 +21,4 @@ export const addItemToCart = (cartItems , cartItemToAdd) => {
      return [...cartItems , { ...cartItemToAdd , quantity : 1}]
 
 }
+
